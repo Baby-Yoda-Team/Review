a0113ef811c6fe395e6e5b02b6d5f51b1ec304a1
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-01-27 00:40:51 -0800

    Product detial page dummy animation added

--- src/components/style.js
+++ src/components/style.js
@@ -3,10 +3,37 @@ import styled from 'styled-components';
 export const ProductDetailsBody = styled.div`
   font-family: Helvetica Neue, Helvetica, Arial, Roboto, sans-serif;
   width: 100%;
+
+  .fade-entry {
+    transition: all 0.2s ease-out;
+  }
+
+  .fade-enter-active {
+    opacity: 0;
+  }
+
+  .fade-enter-done {
+    opacity: 1;
+  }
+
+  .fade-exit {
+    opacity: 1;
+  }
+
+  .fade-exit-active {
+    opacity: 0;
+    transition: opacity 0.2s ease-in;
+  }
+
+  .fade-exit-done {
+    opacity: 0;
+  }
 `;
 
 export const Switcher = styled.div`
   display: flex;
+  align-items: baseline;
+  box-sizing: content-box;
   justify-content: space-between;
   font-size: 20px;
   padding: 10px;
@@ -66,14 +93,6 @@ export const LineStyle = styled.div`
     line-height: 28px;
     padding-bottom: 30px;
   }
-  .bolderOnly {
-    margin-top: 0;
-    font-weight: bold;
-  }
-  .size {
-    font-size: 10px;
-    opacity: 0.6;
-  }
 `;
 
 export const TitleSize = styled.div`
@@ -83,6 +102,8 @@ export const TitleSize = styled.div`
 export const ALink = styled.a.attrs({ href: 'http://www.P65Warnings.ca.gov' })`
   text-decoration: none;
   color: #0060a9;
+  font-weight: ${props => (props.bold ? 'bolder' : '0')};
+
   :hover {
     text-decoration: underline;
   }
@@ -142,7 +163,6 @@ export const MixPicAndTextLine = styled.div`
 `;
 
 export const UserManual = styled.div`
-  height: 110px;
   display: flex;
   .PDF {
     min-width: 72px;
@@ -153,15 +173,42 @@ export const UserManual = styled.div`
 
 export const Bottom = styled.div`
   border-bottom: 1px solid #333;
+  margin-top: 30px;
+  opacity: 0.6;
 `;
 
-// export const TitleSize = styled.div`
-//   width: 100%;
-// `;
+export const Button = styled.button`
+  background: ${props => (props.primary ? 'palevioletred' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'palevioletred')};
 
-// export const TitleSize = styled.div`
-//   width: 100%;
-// `;
+  background: ${props => (props.danger ? 'red' : 'white')};
+  color: ${props => (props.primary ? 'white' : 'red')};
+
+  margin-right: ${props => (props.rightDistance ? '10px' : '0')};
+
+  font-size: 16px;
+  font-weight: bolder;
+  color: #333;
+  background-color: #fff;
+  height: 45px;
+  width: 70px;
+  border-radius: 5px;
+  outline: none;
+  cursor: pointer;
+`;
+
+export const Lines = styled.div`
+  .bolderOnly {
+    font-weight: bold;
+    padding: 0 10px;
+    margin-top: 0;
+  }
+  .size {
+    font-size: 10px;
+    opacity: 0.6;
+    padding: 0 10px;
+  }
+`;
 
 // export const TitleSize = styled.div`
 //   width: 100%;
