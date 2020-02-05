f6b21d5c4d5f456f8ebaad06e6460462c95764b0
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-05 15:05:01 -0800

    debugging

--- serve/index.js
+++ serve/index.js
@@ -8,6 +8,16 @@ const bodyParser = require('body-parser'); /*  middleware that handle post from 
 
 const Mongo = require('../db/index');
 
+const port = 8081;
+
+const originUrl = '*';
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', originUrl);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.json()); // conveart to json file
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, '../dist')));
@@ -22,16 +32,4 @@ app.get('/allData', (request, response) => {
   });
 });
 
-const service = app.listen(8081, () => {
-  const host = service.address().address;
-
-  const port = service.address().port.toString();
-
-  console.log('service port is http://127.0.0.1', port);
-
-  // http://127.0.0.1:8081 /
-});
-
-// let server;
-// const start = () => (server = app.listen(8081, () => {}));
-// const close = server ? server.close : () => {};
+app.listen(port, () => console.log(`listening on port ${port}!`));
